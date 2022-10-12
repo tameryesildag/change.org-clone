a0cf7f5baf84b586aca99bf7d05fdab3ed8c5a52
Tamer <tameryesildag321@gmail.com>
2022-10-12 23:29:18 +0300

    bug fix & drop down menu

--- frontend/src/components/MainNavigator/MainNavigator.js
+++ frontend/src/components/MainNavigator/MainNavigator.js
@@ -1,29 +1,37 @@
 import { Link } from "react-router-dom";
 import styles from "./MainNavigator.module.css";
 import logo from "../../assets/navlogo.png";
-import searchLogo from "../../assets/searchlogo.png";
+import searchLogo from "../../assets/searchicon.png";
 import { useContext } from "react";
 import AuthContext from "../../contexts/AuthContext";
+import userImage from "../../assets/user.png";
 
 function MainNavigator() {
 
     const authValues = useContext(AuthContext);
 
-    function logoutHandler(){
+    function logoutHandler() {
         authValues.setToken(null);
+    }
+
+    function dropdownHandler() {
+        let element = document.getElementById("dropdown-content");
+        console.log(element.style.display);
+        if(element.style.display === "block") return element.style.display = "none";
+        element.style.display = "block";
     }
 
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
-                <div className={styles.logo}>
+                <div className={styles["logo-container"]}>
                     <Link to="/">
                         <img src={logo}></img>
                     </Link>
                 </div>
                 <ul className={styles["nav-list-left"]}>
                     <li>
-                        {authValues.token ? <Link to="/start-a-petition">Start a Petition</Link> : <Link to="/login-or-register">Start a Petition</Link>} 
+                        {authValues.token ? <Link to="/start-a-petition">Start a Petition</Link> : <Link to="/login-or-register">Start a Petition</Link>}
                     </li>
                     <li>
                         <Link to="/user/me">My Petitions</Link>
@@ -38,11 +46,16 @@ function MainNavigator() {
                 <ul className={styles["nav-list-right"]}>
                     <li>
                         <Link to="/search">
-                            <img className={styles["search-logo"]} src={searchLogo}></img>
+                            <img className={styles["search-icon"]} src={searchLogo}></img>
                         </Link>
                     </li>
                     <li>
-                        {authValues.token ? <div onClick={logoutHandler}>Log out</div> : <Link to="/login-or-register">Log in</Link>}
+                        {true ? <div id="dropdown" onClick={dropdownHandler} className={styles.dropdown}>
+                            <img className={styles["profile-icon"]} src={userImage}></img>
+                            <div id="dropdown-content" className={styles["dropdown-content"]}>
+                                <p>Test</p>
+                            </div>
+                        </div> : <Link to="/login-or-register">Log in</Link>}
                     </li>
                 </ul>
             </nav>
